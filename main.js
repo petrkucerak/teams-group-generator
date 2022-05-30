d571867278833c7fafe2f4963628f10f690dbb05
petrkucerak <petr@khome.cz>
2022-05-30 22:32:04 +0200

    Create a simple script

--- main.js
+++ main.js
@@ -1,3 +1,45 @@
+/**
+ * Simple function to shuffle an array
+ * @param {*} array
+ */
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+
+/**
+ * The prime project's function for building a solution.
+ */
 function build() {
-  console.log("build");
+  console.log("Start building");
+
+  /* Fetch data from html document */
+  const input = document.getElementById("input").value;
+  const personInGroup = document.getElementById("group-quantity").value;
+
+  /* Parse text into html object */
+  const parser = new DOMParser();
+  const htmlDoc = parser.parseFromString(input, "text/html");
+
+  /* Get all elements with meeting attendees */
+  const participantClass = htmlDoc.getElementsByClassName("ts-user-name");
+
+  /* Save names as an array */
+  let names = new Array();
+  for (let i = 0; i < participantClass.length; ++i) {
+    names.push(participantClass[i].title);
+  }
+
+  /* Shuffle names in an array */
+  shuffle(names);
+
+  /* Create html code with a solution */
+  let out = "<ul>";
+  for (let i = 0; i < names.length; ++i) {
+    let group = Math.floor(i / personInGroup) + 1;
+    out += `<li>${names[i]} - ${group}</li>`;
+  }
+  out += "</ul>";
+
+  /* Render html code */
+  document.getElementById("main").innerHTML = out;
 }
